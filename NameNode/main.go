bb73e11a0b74846fd01f0e4d96eae0fcee3921e7
Sofia Mañana <sofiamanana@sansano.usm.cl>
2021-11-06 09:38:28 -0300

    namenode existe

---
+++ NameNode/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	pb "Lab2_SD/Pb"
+	"context"
+	"google.golang.org/grpc"
+	"log"
+)
+
+var path = "/ruta_del_archivo/prueba.txt"
+
+func crearArchivo() {
+	//Verifica que el archivo existe
+	var _, err = os.Stat(path)
+	//Crea el archivo si no existe
+	if os.IsNotExist(err) {
+	  var file, err = os.Create(path)
+	  if existeError(err) {
+		return
+	  }
+	  defer file.Close()
+	}
+	fmt.Println("File Created Successfully", path)
+  }
+
+  func escribeArchivo() {
+	// Abre archivo usando permisos READ & WRITE
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	if existeError(err) {
+	  return
+	}
+	defer file.Close()
+	// Escribe algo de texto linea por linea
+	_, err = file.WriteString("Hola \n")
+	if existeError(err) {
+	  return
+	}
+	_, err = file.WriteString("Mundo \n")
+	if existeError(err) {
+	  return
+	}
+	// Salva los cambios
+	err = file.Sync()
+	if existeError(err) {
+		return
+	  }
+	  fmt.Println("Archivo actualizado existosamente.")
+	}
+	func existeError(err error) bool {
+	  if err != nil {
+		fmt.Println(err.Error())
+	  }
+	  return (err != nil)
+	}
